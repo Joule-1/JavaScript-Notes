2c707d8e3bfb12478fe833c2d9bc624d87a84afa
Akshay Pandey <172425800+Joule-1@users.noreply.github.com>
2024-08-02 00:31:55 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,20 @@
+function first(a,b){ // parameters
+    console.log("hi");
+    // console.log(a-b);
+    return a-b;
+}
+
+// res = first(1,2);  // arguments
+// console.log(res);
+
+function check(va1, val22, ...num){ 
+    return num
+}
+
+// console.log(check(100,200))
+
+const addTwo = (num1, num2) => {
+     return num1 + num2
+}
+
+const adddTwo = (num1, num2) =>  num1 + num2
