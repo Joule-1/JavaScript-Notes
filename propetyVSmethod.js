ffc00b87221a23129488ad7478e7ea873552200a
Akshay Pandey <172425800+Joule-1@users.noreply.github.com>
2024-08-02 11:14:08 +0530

    Add files via upload

---
+++ propetyVSmethod.js
@@ -0,0 +1,38 @@
+let object = {
+    propertyName: 'propertyValue'
+};
+
+/*
+
+Properties are values associated with an 
+object. They define the state or 
+characteristics of an object.
+
+Access:
+
+Dot notation: object.propertyName
+Bracket notation: object['propertyName']
+*/
+
+let object1 = {
+    methodName: function() {
+        // function body
+    }
+};
+
+/*Access:
+
+Dot notation: object1.methodName()
+Bracket notation: object1['methodName']()
+
+*/
+
+
+/*
+Methods are functions that are defined 
+inside an object. They define the behavior
+or actions that can be performed on the 
+object.
+*/
+
+
